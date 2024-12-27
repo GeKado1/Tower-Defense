077027a0f9c7640f4f597d1cb8b85a174d5fd442
GeKado1 <98519650+GeKado1@users.noreply.github.com>
2024-12-27 11:24:40 +0100

    needs some adjustments

--- Assets/Scripts/WaveScripts/WaveSpawner.cs
+++ Assets/Scripts/WaveScripts/WaveSpawner.cs
@@ -17,6 +17,8 @@ public class WaveSpawner : MonoBehaviour {
     private float countdown = 0;
     private int waveNum = 0;
 
+    private bool isSpawning;
+
     private bool initialWave = true;
 
     // Start is called before the first frame update
@@ -37,28 +39,46 @@ public class WaveSpawner : MonoBehaviour {
             return;
         }
 
-        if (waveNum == waves.Length && GameManager.gameEnd != true) {
-            if (!GameManager.hardMode) {
+        if (!GameManager.hardMode) {
+            if (waveNum == waves.Length && GameManager.gameEnd != true) {
+                gameManager.WinLevel();
+                enabled = false;
+
+                //Debug.Log("no hauria de entra aqui");
+            }
+
+            if (countdown <= 0f && initialWave != true) {
+                StartCoroutine(SpawnWave());
+
+                Debug.Log("Starting wave " + waveNum);
+
+                countdown = timeBetweenWaves;
+                return;
+            }
+        }
+        else {
+            Debug.Log("entra aqui");
+
+            if (enemiesAlive <= 0 && waveNum >= waves.Length) {
+                Debug.Log("win hard");
                 gameManager.WinLevel();
                 enabled = false;
             }
-            else {
-                if (enemiesAlive <= 0) {
-                    gameManager.WinLevel();
-                    enabled = false;
+
+            if (!isSpawning && waveNum < waves.Length) {
+                if (countdown <= 0f && initialWave != true) {
+                    StartCoroutine(SpawnWave(waves[waveNum]));
+
+                    Debug.Log("Starting wave " + waveNum);
+
+                    countdown = timeBetweenWaves;
+                    return;
                 }
             }
-            
         }
 
         if (GameManager.gameEnd == true) {
             enabled = false;
-        }
-
-        if (countdown <= 0f && initialWave != true) {
-            StartCoroutine(SpawnWave());
-            countdown = timeBetweenWaves;
-            return;
         }
 
         countdown -= Time.deltaTime;
@@ -70,14 +90,40 @@ public class WaveSpawner : MonoBehaviour {
     IEnumerator SpawnWave() {
         PlayerStats.rounds++;
 
-        Wave wave = waves[waveNum];
-        enemiesAlive = wave.count * spawnPoints.Length;
+        if (waveNum < waves.Length) {
+            Wave wave = waves[waveNum];
+
+            Debug.Log("wave num: " + waveNum + " waves length: " + waves.Length);
+
+            if (!GameManager.hardMode) {
+                enemiesAlive = wave.count * spawnPoints.Length;
+            }
+            /*else {
+                enemiesAlive += wave.count * spawnPoints.Length;
+            }*/
+
+
+            for (int i = 0; i < wave.count; i++) {
+                //Debug.Log("i: " + i + " wave: " + wave.count);
+                SpawnEnemy(wave.enemy);
+                yield return new WaitForSeconds(1f / wave.rate);
+            }
+
+            waveNum++;
+        }
+    }
+
+    IEnumerator SpawnWave(Wave wave) {
+        isSpawning = true;
+
+        enemiesAlive += wave.count * spawnPoints.Length;
 
         for (int i = 0; i < wave.count; i++) {
             SpawnEnemy(wave.enemy);
-            yield return new WaitForSeconds(1f/wave.rate);
+            yield return new WaitForSeconds(1f / wave.rate);
         }
-        
+
+        isSpawning = false;
         waveNum++;
     }
 
