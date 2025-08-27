d67acae5dece92f250aa611e30f8f060e43d02cc
GeKado1 <98519650+GeKado1@users.noreply.github.com>
2025-08-27 11:52:19 +0200

    resolutions

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,38 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
     [SerializeField] private string levelSelector = "LevelSelector";
-    [SerializeField] SceneFader sceneFader;
+    [SerializeField] private SceneFader sceneFader;
     [SerializeField] private GameObject settings;
+
+    [SerializeField] private TMPro.TMP_Dropdown resolutionsDropdown;
+
+    private Resolution[] resolutions;
 
     private void Start() {
         settings.SetActive(false);
+
+        resolutions = Screen.resolutions;
+
+        resolutionsDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++) {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height) {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionsDropdown.AddOptions(options);
+        resolutionsDropdown.value = currentResolutionIndex;
+        resolutionsDropdown.RefreshShownValue();
     }
 
     public void Play() {
@@ -23,6 +49,11 @@ public class MainMenu : MonoBehaviour {
 
     public void SetQuality(int qualityIndex) {
         QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetResolution(int resolutionIndex) {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
     public void SetFullscreen(bool isFullscreen) {
